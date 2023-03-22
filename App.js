4f863570274df45a00a6cf0a9fc48fea46e3c2b1
ashish-monal <aviashishranjan@gmail.com>
2023-03-22 16:23:37 +0530

    Advice App

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+
+  const getRandomId = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return (Math.floor(Math.random() * (max - min + 1)) + min).toString();
+  };
+
+  const getAdvice = () => {
+    axios
+      .post("http://api.adviceslip.com/advice/" + getRandomId(1, 200))
+      .then((response) => {
+        console.log(response);
+        setAdvice(response.data.slip.advice);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.advice}>{advice}</Text>
+      <Button title="get Advice" onPress={getAdvice} color="green" />
     </View>
   );
 }
@@ -13,8 +31,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  advice: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginHorizontal: 20,
   },
 });
